6460b8fbb46cef5a7eb85c672af1df66ad6e2292
anggoro <75475767+anggoroexe@users.noreply.github.com>
2022-04-26 20:58:57 +0700

    Create bacaquran.sh

---
+++ bacaquran.sh
@@ -0,0 +1,36 @@
+list=$(curl -s https://al-quran-8d642.firebaseio.com/data.json?print=pretty | jq -r '.[] | "\(.nomor) : \(.nama)"')
+banner=$(echo '''
+
+╭━━━╮╱╱╱╱╱╱╭━━━╮
+┃╭━╮┃╱╱╱╱╱╱┃╭━╮┃
+┃┃╱┃┣╮╱╭┳━━┫┃╱┃┣╮╭┳━┳━━┳━╮
+┃╰━╯┃┃╱┃┃╭╮┃┃╱┃┃┃┃┃╭┫╭╮┃╭╮╮
+┃╭━╮┃╰━╯┃╰╯┃╰━╯┃╰╯┃┃┃╭╮┃┃┃┃
+╰╯╱╰┻━╮╭┻━━┻━━╮┣━━┻╯╰╯╰┻╯╰╯
+╱╱╱╱╭━╯┃╱╱╱╱╱╱╰╯  Ayo Baca Al Quran
+╱╱╱╱╰━━╯
+
+Menu :
+1. List Surah 
+2. Baca Surah
+''')
+echo "${banner}"
+read -p "Pilih Menu :" pilihan
+if [ $pilihan = "1" ]
+then
+echo "List Nomer Surah dan Nama Surah"
+echo "${list}"
+sleep 15s
+echo "${banner}"
+read -p "Pilih Menu :" pilihan
+fi
+if [ $pilihan = "2" ]
+then
+read -p "Pilih Nomer Surah :" surah 
+quran=$(curl -s https://al-quran-8d642.firebaseio.com/surat/${surah}.json?print=pretty | jq -r '.[] | "\(.tr) : \(.id)"' | sed -r 's/[\<][\/]?[a-zA-Z0-9\=\"\-\#\.\& ]+[\/]?[\>]//g')
+rilldata=$(( $surah - 1 ))
+judul=$(curl -s https://al-quran-8d642.firebaseio.com/data.json?print=pretty | jq -r '.['${rilldata}'] | "\(.nama)"')
+ayat=$(curl -s https://al-quran-8d642.firebaseio.com/data.json?print=pretty | jq -r '.['${rilldata}'] | "\(.ayat)"')
+echo "surah ${judul} jumlah ayat ${ayat}"
+echo "${quran}"
+fi
